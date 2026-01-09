0c2262e4188049e6ddc908043718cd2ef88dfc20
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-09 12:57:09 +0530

    Used async and await to make code more readable

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "endMarks.txt");
 
-fs.readFile(filePath, "utf-8", (err, data) => {
-  console.log("CGPA of each subject in the Odd Sem of 2nd year: ");
+function BFileRead() {
+  return new Promise((resolve) => {
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      resolve(data);
+    });
+  });
+}
+
+async function main() {
+  const data = await BFileRead();
+  console.log("Each subject CGPA in the odd sem of the 2nd year:");
   console.log(data);
-});
+}
+
+main();
+
